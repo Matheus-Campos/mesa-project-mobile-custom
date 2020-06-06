903f3115129a50a958ebc0edeae52d0bffe8be66
Matheus Campos da Silva <silva.campos.matheus@gmail.com>
2020-06-06 11:15:17 -0300

    adds react-native-vector-icons module; implements tabs navigator; create profile screen.

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,45 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import {navigationRef} from './services/navigation';
 
 import SignInScreen from './screens/SignInScreen';
 import SignUpScreen from './screens/SignUpScreen';
 import MainScreen from './screens/MainScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const Tabs = () => (
+  <Tab.Navigator
+    tabBarOptions={{
+      activeTintColor: '#000',
+      inactiveTintColor: 'rgba(0, 0, 0, 0.5)',
+      labelStyle: {fontSize: 14},
+      tabStyle: {}
+    }}>
+    <Tab.Screen
+      name="Main"
+      component={MainScreen}
+      options={{
+        title: 'Locais',
+        tabBarIcon: ({color}) => <Icon name="map" size={28} color={color} />,
+      }}
+    />
+    <Tab.Screen
+      name="Profile"
+      component={ProfileScreen}
+      options={{
+        title: 'Perfil',
+        tabBarIcon: ({color}) => <Icon name="person" size={28} color={color} />,
+      }}
+    />
+  </Tab.Navigator>
+);
 
 const Routes = ({auth}) => {
   if (!auth.authChecked) {
@@ -22,7 +53,7 @@ const Routes = ({auth}) => {
         {auth.isSignedIn ? (
           <Stack.Screen
             name="Main"
-            component={MainScreen}
+            component={Tabs}
             options={{headerShown: false}}
           />
         ) : (
